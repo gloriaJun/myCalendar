fb83f2086cc19a4157a70f8f7aed8b5c4deac044
gloria <pureainu@gmail.com>
2017-04-11 18:42:10 +0900

    add unit-test draft

--- todo-app/src/test/java/com/study/todo/contorller/ToDoControllerTest.java
+++ todo-app/src/test/java/com/study/todo/contorller/ToDoControllerTest.java
@@ -1,11 +1,21 @@
 package com.study.todo.contorller;
 
 import com.study.todo.ToDoApplication;
+import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = ToDoApplication.class)
@@ -13,5 +23,18 @@ import org.springframework.test.web.servlet.MockMvc;
 public class ToDoControllerTest {
 
     private MockMvc mockMvc;
+    @Autowired
+    private WebApplicationContext ctx;
 
+    @Before
+    public void setup() {
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(ctx).build();
+    }
+
+    @Test
+    public void testToDoList() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/").accept(MediaType.APPLICATION_JSON_UTF8))
+                .andExpect(status().isOk())
+                .andExpect(MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON_UTF8));
+    }
 }
