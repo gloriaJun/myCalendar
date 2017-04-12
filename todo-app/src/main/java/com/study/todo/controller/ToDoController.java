95ebe3b74b0dd96c7a812afbf289561d69ced541
gloria <gloria@gloria-MacPro.local>
2017-04-13 00:44:05 +0900

    init setting about swagger2

--- todo-app/src/main/java/com/study/todo/controller/ToDoController.java
+++ todo-app/src/main/java/com/study/todo/controller/ToDoController.java
@@ -1,14 +1,25 @@
 package com.study.todo.controller;
 
+import com.study.todo.model.ToDos;
+import io.swagger.annotations.ApiOperation;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("/todo")
 public class ToDoController {
 
+    private static final Logger logger = LoggerFactory.getLogger(ToDoController.class);
+
+    @ApiOperation(value = "Get ToDo List")
     @GetMapping
-    public String getToDoList() {
-        return "test";
+    public List<ToDos> getToDoList() {
+        List<ToDos> toDosList = new ArrayList<ToDos>();
+        return toDosList;
     }
 
     @PostMapping
---
+++ todo-app/src/main/java/com/study/todo/model/ToDos.java
@@ -0,0 +1,7 @@
+package com.study.todo.model;
+
+/**
+ * Created by gloria on 2017. 4. 12..
+ */
+public class ToDos {
+}
